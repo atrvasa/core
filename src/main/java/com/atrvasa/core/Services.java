a9f02ad6f920e9978b640a7135f7c5427861ce75
Keykhosrow <atrvasa@gmail.com>
2023-01-13 10:21:19 +0330

    Init core project

---
+++ src/main/java/com/atrvasa/core/Services.java
@@ -0,0 +1,30 @@
+/*
+ * MIT License (https://github.com/atrvasa/infra-service/blob/main/LICENSE)
+ * Repository (https://github.com/atrvasa/core)
+ */
+
+package com.atrvasa.core;
+
+import com.atrvasa.infra.service.BaseServices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Defines a set of delegates and services.
+ *
+ * @author Keykhosrow
+ * <p>
+ * {@link com.atrvasa.infra.service.BaseServices}.
+ */
+public class Services extends BaseServices {
+
+
+    @Override
+    public List<Class<?>> getServices() {
+        List<Class<?>> servicesTypes = new ArrayList<>();
+
+
+        return servicesTypes;
+    }
+}
